8b1818f1ccb29e7154f17e6c9ac30e249896bba3
Bill Klinkow <klinkow@gmail.com>
2018-01-11 14:04:36 -0800

    add ajax request and basic weather reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,11 +6,14 @@ const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_K
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
+  console.log('got before axios request');
   const url = `${ROOT_URL}&q=${city},us`;
   const request = axios.get(url);
+
+  console.log('Request:', request)
 
   return {
     type: FETCH_WEATHER,
     payload: request
-  }
+  };
 }
